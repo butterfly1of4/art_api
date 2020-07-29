40e0b689730405a9e81a85b8f62ebe34e2ce8d18
butterfly1of4 <jhouck29@gmail.com>
2020-07-29 19:01:02 -0400

    model?

---
+++ models/Records.js
@@ -0,0 +1,20 @@
+const mongoose = require('../db/connection')
+
+const Schema = mongoose.Schema 
+const People = new Schema({
+    rold: String,
+    birthplace: String,
+    name: String,
+    personid: Number,
+    deathplace: String
+})
+const Records = new Schema ({   title: String,
+    id: Number,
+    classification: String,
+    url: String,
+    century: String,
+    culture: String, 
+    medium: String,
+    people: [People]
+})
+module.exports = mongoose.model('Records', Records)
