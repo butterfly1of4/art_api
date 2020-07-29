75c9775f9aef99c6c9adae92034a4f8aec22bcae
butterfly1of4 <jhouck29@gmail.com>
2020-07-29 14:43:26 -0400

    progress

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,83 @@
 //axios or fetch
 const axios = require('axios').default;
-const fs = require('fs')
+// const fetch = require("node-fetch");
+const fs = require('fs');
 
-const url = "https://api.harvardartmuseums.org//person/PERSON_ID?apikey=xfebeaa16-0d03-4897-9c68-6b012cec30cd"
+// q=ulan_id:500011328?
+const url = "https://api.harvardartmuseums.org//person?apikey=febeaa16-0d03-4897-9c68-6b012cec30cd"
 
-fetch(url)
-  .then((res) => res.json())
-  .then((res) => {
-    console.log(res);
-    let countries = JSON.stringify(res);
-    console.log(countries);
-    fs.writeFile("./db/data.json", countries, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("success");
-      }
-    });
-  });
+let works = ''
+
+axios.get(url)
+  .then(function (response) {
+    works = JSON.stringify(response)
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(  
+      
+      
+    fs.writeFile("./lib/data.json", works, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("success");
+          }
+        })
+  );
+    
+
+
+
+
+
+
+
+
+
+
+// axios.get(url)
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
+
+
+
+
+
+// var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
+// var queryString = {$.param({
+//     apikey: "xfebeaa16-0d03-4897-9c68-6b012cec30cd",
+//     ulan_id: "500011328",
+//     classification: "People"
+// })};
+
+// $.getJSON(apiEndpointBaseURL + "?" + queryString, function(data) {
+//    console.log(data); 
+// });
+
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((res) => {
+//     console.log(res);
+//     let works = JSON.stringify(res);
+//     console.log(works);
+//     fs.writeFile("./lib/data.json", works, (err) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log("success");
+//       }
+//     });
+//   });
