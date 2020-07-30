c27531c716f9e426062379da3ae5367a020f3fe0
butterfly1of4 <jhouck29@gmail.com>
2020-07-30 10:06:01 -0400

    seeded

--- db/seed.js
+++ db/seed.js
@@ -3,27 +3,27 @@ const data = require("../lib/data.json");
 const newJSON = require("./getRecords")
 
 
-// const manyRecords = data.map((item) => {
-//   const record = {};
-//   record.title = item.title;
-//   record.id = item.id;
-//   record.classification = item.classification;
-//   record.url = item.url;
-//   record.century = item.century;
-//   record.culture = item.culture;
-//   record.medium = item.medium;
-//   return record;
-// });
+const manyRecords = data.records.map((item) => {
+  const record = {};
+  record.title = item.title;
+  record.id = item.id;
+  record.classification = item.classification;
+  record.url = item.url;
+  record.century = item.century;
+  record.culture = item.culture;
+  record.medium = item.medium;
+  return record;
+});
 
-// console.log(manyRecords)
+console.log(manyRecords)
 
-// Records.deleteMany({}).then(() => {
-//   Records.create(manyRecords).then((records) => {
-//     console.log(records);
-//     process.exit();
-//   })
-//   .catch(err => {
-//       console.log(err)
-//       process.exit()
-//   } );
-// });
+Records.deleteMany({}).then(() => {
+  Records.create(manyRecords).then((records) => {
+    console.log(records);
+    process.exit();
+  })
+  .catch(err => {
+      console.log(err)
+      process.exit()
+  } );
+});
