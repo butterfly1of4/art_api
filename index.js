3e04ca8c8ff5d7755be8a77aee181bb332340235
butterfly1of4 <jhouck29@gmail.com>
2020-07-30 11:43:27 -0400

    post works

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
 const Records = require("./models/Records");
+const data = require("./lib/data.json");
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
@@ -13,34 +14,35 @@ app.get("/record", (req, res) => {
   });
 });
 
-app.get("/record/title", (req, res) => {
-  Records.findOne({ title: req.params.title }).then((record) => {
+app.get("/record/:title", (req, res) => {
+  Records.find({ title: req.params.title }).then((record) => {
     res.json(record);
   });
 });
 
-app.post("/record", (req, res) => {
+app.post("/record",  (req, res) => {
   console.log("req.body", req.body);
   Records.create(req.body).then((record) => {
+    console.log("hello")
     res.json(record);
   });
 });
 
-app.put("/records/:id/item/:item", (req, res) => {
-  Records.findByIdAndUpdate(
-    req.params.id,
-    { $push: { items: req.body } },
-    { new: true }
-  ).then((record) => {
-    res.json(record);
-  });
-});
+// app.put("/records/:id/item/:item", (req, res) => {
+//   Records.findByIdAndUpdate(
+//     req.params.id,
+//     { $push: { items: req.body } },
+//     { new: true }
+//   ).then((record) => {
+//     res.json(record);
+//   });
+// });
 
-app.delete("/record/:id", (req, res) => {
-  Records.findByIdAndRemove({ _id: req.params.id }).then((record) => {
-    res.json(record);
-  });
-});
+// app.delete("/record/:id", (req, res) => {
+//   Records.findByIdAndRemove({ _id: req.params.id }).then((record) => {
+//     res.json(record);
+//   });
+//});
 
 app.listen(3000, () => {
   console.log("listening");
