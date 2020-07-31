346d290e6f619869a3bb974e7e5e9e3a863def15
butterfly1of4 <jhouck29@gmail.com>
2020-07-31 17:28:10 -0400

    Revert "ok"
    This reverts commit 7cc794776628cbcf8b0cf393948446e15731a5fb.

--- art_api/src/App.js
+++ art_api/src/App.js
@@ -1,57 +1,59 @@
-import React from "react";
-import { Route, Link, Redirect } from "react-router-dom";
-import logo from "./logo.svg";
-import "./App.css";
-import { response } from "express";
+import React from 'react';
+import {Route, Link, Redirect} from "react-router-dom"
+import logo from './logo.svg';
+import './App.css';
+import { response } from 'express';
 
 const url =
   "https://api.harvardartmuseums.org/object?q=culture:Italian&apikey=208241b1-775f-4706-8f78-b382d1981b11";
 
 const entryGet = {
-  method: "GET",
-  headers: {
-    Accept: "data/json",
-  },
-};
+    method: "GET",
+    headers: {
+        "Accept" : 'data/json'
+    }
+}
 const entryPut = {
-  method: "PUT",
-  headers: {
-    Accept: "data/json",
-  },
-};
+    method: "PUT",
+    headers: {
+        "Accept" : 'data/json'
+    }
+}
 const entryPost = {
-  method: "POST",
-  headers: {
-    Accept: "data/json",
-  },
-};
+    method: "POST",
+    headers: {
+        "Accept" : 'data/json'
+    }
+}
 const entryDelete = {
-  method: "DELETE",
-  headers: {
-    Accept: "data/json",
-  },
-};
+    method: "DELETE",
+    headers: {
+        "Accept" : 'data/json'
+    }
+}
 
-class App extends Component() {
-  constructor(props) {
-    super(props);
-    this.state = {
-      entry: [],
-    };
-  }
-  componentDidMount() {
-    fetch(url, entryGet)
-      .then((res) => res.json())
-      .then((res) => {
-        this.state({
-          entry: res.entry,
-        });
-        return this.state.entry;
-      });
-  }
-  render() {
-    return <div></div>;
-  }
+
+
+class App extends Component( ){
+    constructor(props){
+        super(props);
+        this.state = {
+            entry: []
+        } 
+        
+    }
+componentDidMount() {
+    fetch(url) 
+    // then res resjson
+    // then convered 
+    // converted responsethis.setState
+}
+render(){
+    return(
+        <div></div>
+
+    )
+}
 }
 
 export default App;
